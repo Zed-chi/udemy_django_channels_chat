72be6d815147d1137668ec1487d5de718c3e12f7
zedchi <zed-chi@mail.ru>
2019-09-21 21:47:32 +0300

    added new route layer handler

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,19 +1,31 @@
-from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
+from channels.generic.websocket import (WebsocketConsumer, AsyncWebsocketConsumer, 
+                                JsonWebsocketConsumer, AsyncJsonWebsocketConsumer)
 from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.exceptions import StopConsumer
+from asgiref.sync import async_to_sync
 import json
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        async_to_sync(self.channel_layer.group_add)(self.room_name,self.channel_name)
         self.accept()
     
     def disconnect(self, code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(self.room_name,self.channel_name)
 
     def receive(self, text_data=None, bytes_data=None):
-        data = json.loads(text_data)
-        message = data["message"]
-        self.send(text_data=json.dumps({"answer":message}))
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_name,
+            {
+                "type":"chat.message",
+                "text": text_data
+            }
+        )
+
+    def chat_message(self, event):
+        self.send(text_data=event["text"])
 
 
 class AsyncChatConsumer(AsyncWebsocketConsumer):
@@ -49,3 +61,41 @@ class BaseAsyncConsumer(AsyncConsumer):
             "type":"websocket.send",
             "text":event['text']
         })
+
+
+class ChatJsonConsumer(JsonWebsocketConsumer):
+    def connect(self):
+        self.accept()
+    
+    def disconnect(self, code):
+        pass
+
+    def receive_json(self, content):
+        self.send_json(content=content)
+    
+    @classmethod
+    def encode_json(cls, content):
+        return super().encode_json(content)
+
+    @classmethod
+    def decode_json(cls, text_data):
+        return super().decode_json(text_data)
+
+
+class ChatAsyncJsonConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+    
+    async def disconnect(self, code):
+        pass
+
+    async def receive_json(self, content):
+        await self.send_json(content=content)
+    
+    @classmethod
+    async def encode_json(cls, content):
+        return super().encode_json(content)
+
+    @classmethod
+    async def decode_json(cls, text_data):
+        return super().decode_json(text_data)    
