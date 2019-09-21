d70828ef3ea9cdfcb78b207730d10a902cf03c20
zedchi <zed-chi@mail.ru>
2019-09-21 21:46:59 +0300

    added layer handler

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .consumers import ChatConsumer, AsyncChatConsumer, BaseSyncConsumer
+from .consumers import ChatConsumer, AsyncChatConsumer, BaseSyncConsumer, BaseAsyncConsumer
 
 urls = [
-    url(r'^ws/chat/$', BaseSyncConsumer),
+    url(r'^ws/chat/$', BaseAsyncConsumer),
+    url(r'^ws/chat/(?P<room_name>\w+)/$', ChatConsumer),
 ]
